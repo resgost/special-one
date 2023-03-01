0c1583ccc60b983eb819efdff1d118351a09f092
resgost <112984909+resgost@users.noreply.github.com>
2023-03-01 09:47:00 -0500

    Add files via upload

---
+++ NBA_MAIN.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import subprocess
+import time
+
+
+# In[ ]:
+
+
+time.sleep(35)
+
+
+# In[3]:
+
+
+file_path = r"C:\Users\Usuario\PORTFOLIO\LA_LIGA\Automa\predicciones.xlsm"
+subprocess.run(["start", file_path], shell=True)
+

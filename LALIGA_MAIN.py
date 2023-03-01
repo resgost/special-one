a1be4a600cd257e6bc672606a50a8952d15047c1
resgost <112984909+resgost@users.noreply.github.com>
2023-03-01 10:31:28 -0500

    Add files via upload

---
+++ LALIGA_MAIN.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import subprocess
+import time
+
+
+# In[ ]:
+
+
+time.sleep(35)
+
+
+# In[3]:
+
+
+file_path = r"D:\Usuario\Descargas\special-one-main\predicciones.xlsm"
+subprocess.run(["start", file_path], shell=True)
+
